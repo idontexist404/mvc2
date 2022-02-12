46d49b45fb4dedb38bb0d51c0b827f822bc95ef1
Highlander Magno <highlander9200@gmail.com>
2022-02-12 17:05:04 -0300

    Added a basic model that 'connects to a fictious db' lol.

--- app/Model/Entity/Organization.php
+++ app/Model/Entity/Organization.php
@@ -1,1 +1,38 @@
 <?php
+
+namespace App\Model\Entity;
+
+/* Essa classe é apenas um 'banco de dados' ficticio para exemplificar como o model pode
+ser implementado */
+
+class Organization
+{
+    /**
+     * ID da organização
+     *
+     * @var intenger
+     */
+    public $id = 1;
+
+    /**
+     * Nome da organização
+     *
+     * @var string
+     */
+    public $name = 'Evil Corp. (•̀ᴗ•́)و ̑̑ ';
+
+    /**
+     * Site da organização
+     *
+     * @var string
+     */
+    public $site = 'https://www.evilcorp.com/';
+
+    /**
+     * Descrição da organizaçao
+     *
+     * @var string
+     */
+    public $description = 'Large super-corporation that is very big. And evil! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quas doloribus corrupti minus sint quibusdam velit, dolorem distinctio neque dignissimos ratione ullam, nulla eius vel ea quod rem accusantium aliquid.';
+
+}
